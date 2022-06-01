19646a991ac36312bca5ef72d59d92dea510f4b4
Natanel Shitrit <natanelsht@gmail.com>
2022-06-01 21:12:30 +0300

    `notification.entry.value.profile` object implementation

--- src/whatsapp_cloud_pyapi/types/notification/entry/value/profile/profile.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/value/profile/profile.py
@@ -3,4 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Profile:
-    pass
+    
+    name: str
+    """
+    The customer's name.
+    """
+
+    wa_id: str
+    """
+    The customer's WhatsApp ID. A business can respond to a message using this ID.
+    """
