5e82ab6d09e288bff6cd89377e1480534c81f9ba
Natanel-Shitrit <natanelsht@gmail.com>
2022-06-02 20:01:12 +0300

    `notification.entry.value.message.image` object implementation

--- src/whatsapp_cloud_pyapi/types/notification/entry/value/message/image/image.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/value/message/image/image.py
@@ -1,6 +1,26 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Image:
-    pass
+    
+    mime_type: str
+    """
+    Mime type for the image.
+    """
+
+    id: str
+    """
+    ID for the image.
+    """
+
+    sha256: str
+    """
+    Image hash.
+    """
+
+    caption: Optional[str] = None
+    """
+    Caption for the image, if provided.
+    """
