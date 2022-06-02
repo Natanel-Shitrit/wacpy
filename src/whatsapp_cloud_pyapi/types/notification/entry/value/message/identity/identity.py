568b3c7ac351ca7c68b8c178126a0448b0a60f35
Natanel-Shitrit <natanelsht@gmail.com>
2022-06-02 20:05:13 +0300

    `notification.entry.value.message.identity` object implementation

--- src/whatsapp_cloud_pyapi/types/notification/entry/value/message/identity/identity.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/value/message/identity/identity.py
@@ -3,4 +3,18 @@ from dataclasses import dataclass
 
 @dataclass
 class Identity:
-    pass
+    
+    acknowledged: str
+    """
+    State of acknowledgment for the messages system customer_identity_changed.
+    """
+
+    created_timestamp: str
+    """
+    The time when the WhatsApp Business Management API detected the customer may have changed their profile information.
+    """
+
+    hash: str
+    """
+    The ID for the messages system customer_identity_changed.
+    """
