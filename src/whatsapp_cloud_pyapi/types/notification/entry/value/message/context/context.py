456a811fa7c3325419b3e2b328f46e696d0f72db
Natanel-Shitrit <natanelsht@gmail.com>
2022-06-02 20:55:25 +0300

    `notification.entry.value.message.context` object implementation

--- src/whatsapp_cloud_pyapi/types/notification/entry/value/message/context/context.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/value/message/context/context.py
@@ -3,4 +3,25 @@ from dataclasses import dataclass
 
 @dataclass
 class Context:
-    pass
+    
+    forwarded: bool
+    """
+    Set to true if the message received by the business has been forwarded.
+    """
+
+    frequently_forwarded: bool
+    """
+    Set to true if the message received by the business has been forwarded more than 5 times.
+    """
+
+    # TODO: when adding `dataclasses_json` use this:
+    # from_: str = field(metadata=config(field_name="from"))
+    from_: str
+    """
+    The WhatsApp ID for the customer who replied to an inbound message.
+    """
+
+    id: str
+    """
+    The message ID for the sent message for an inbound reply.
+    """
