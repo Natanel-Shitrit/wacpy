e13b7f61f94f2d299e4e1036caa214805f42bef1
Natanel-Shitrit <natanelsht@gmail.com>
2022-06-02 11:57:50 +0300

    `notification.entry.value.metadata` object implementation

--- src/whatsapp_cloud_pyapi/types/notification/entry/value/metadata/metadata.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/value/metadata/metadata.py
@@ -3,4 +3,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Metadata:
-    pass
+    
+    display_phone_number: str
+    """
+    The phone number that is displayed for a business.
+    """
+
+    phone_number_id: str
+    """
+    ID for the phone number.
+    A business can respond to a message using this ID.
+    """
