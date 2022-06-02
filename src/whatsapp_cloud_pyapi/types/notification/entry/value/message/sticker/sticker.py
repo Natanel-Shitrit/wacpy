1b6f972c7721630a464e243277dd0d1c203b8548
Natanel-Shitrit <natanelsht@gmail.com>
2022-06-02 16:45:57 +0300

    `notification.entry.value.message.sticker` object implementation

--- src/whatsapp_cloud_pyapi/types/notification/entry/value/message/sticker/sticker.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/value/message/sticker/sticker.py
@@ -3,4 +3,18 @@ from dataclasses import dataclass
 
 @dataclass
 class Sticker:
-    pass
+    
+    mime_type: str
+    """
+    image/webp.
+    """
+
+    sha256: str
+    """
+    Hash for the sticker.
+    """
+
+    id: str
+    """
+    ID for the sticker.
+    """
