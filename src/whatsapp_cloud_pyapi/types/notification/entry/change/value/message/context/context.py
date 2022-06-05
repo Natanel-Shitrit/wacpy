d89d384b2b8040780f2fcb17a97af9645bd1525c
Natanel-Shitrit <natanelsht@gmail.com>
2022-06-05 14:52:38 +0300

    Fix errors on replied messages

--- src/whatsapp_cloud_pyapi/types/notification/entry/change/value/message/context/context.py
+++ src/whatsapp_cloud_pyapi/types/notification/entry/change/value/message/context/context.py
@@ -1,20 +1,11 @@
 from dataclasses_json import dataclass_json, config
 from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass_json
 @dataclass
 class Context:
-    
-    forwarded: bool
-    """
-    Set to true if the message received by the business has been forwarded.
-    """
-
-    frequently_forwarded: bool
-    """
-    Set to true if the message received by the business has been forwarded more than 5 times.
-    """
 
     from_: str = field(metadata=config(field_name="from"))
     """
@@ -24,4 +15,14 @@ class Context:
     id: str
     """
     The message ID for the sent message for an inbound reply.
+    """
+
+    forwarded: Optional[bool] = None
+    """
+    Set to true if the message received by the business has been forwarded.
+    """
+
+    frequently_forwarded: Optional[bool] = None
+    """
+    Set to true if the message received by the business has been forwarded more than 5 times.
     """
