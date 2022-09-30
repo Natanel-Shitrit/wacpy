70eb587e8549b6073b9c47cbe21c1cc92168ffc3
Natanel Shitrit <natanelsht@gmail.com>
2022-09-30 22:20:02 +0300

    Make `name` field in `profile` object optional

--- src/wacpy/types/notification/entry/change/value/contact/profile/profile.py
+++ src/wacpy/types/notification/entry/change/value/contact/profile/profile.py
@@ -1,12 +1,13 @@
-from dataclasses_json import dataclass_json
-from dataclasses import dataclass
+from dataclasses_json import dataclass_json, config
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass_json
 @dataclass
 class Profile:
     
-    name: str
+    name: Optional[str] = field(default=None, metadata=config(exclude=lambda f: f is None))
     """
     The customer's name.
     """
