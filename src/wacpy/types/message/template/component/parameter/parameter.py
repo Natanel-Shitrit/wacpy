ddd3eedc6d34d3f50d190afa9e09f407120a5067
Natanel-Shitrit <natanelsht@gmail.com>
2022-10-02 08:46:49 +0300

    Fix Missing properties in `Message.Template.Component.Parameter` object #8

--- src/wacpy/types/message/template/component/parameter/parameter.py
+++ src/wacpy/types/message/template/component/parameter/parameter.py
@@ -1,3 +1,7 @@
+from .currency import Currency
+from .datetime import DateTime
+from ....media import Media
+
 from dataclasses_json import config, dataclass_json
 from dataclasses import dataclass, field
 from typing import Optional
@@ -31,4 +35,46 @@ class Parameter:
 
     For the header component, the character limit is 60 characters.
     For the body component, the character limit is 1024 characters.
+    """
+
+    currency: Optional[Currency] = field(default=None, metadata=config(exclude=lambda f: f is None))
+    """
+    Required when type=currency.
+
+    A currency object.
+    """
+
+    date_time: Optional[DateTime] = field(default=None, metadata=config(exclude=lambda f: f is None))
+    """
+    Required when type=date_time.
+
+    A date_time object.
+    """
+
+    image: Optional[Media] = field(default=None, metadata=config(exclude=lambda f: f is None))
+    """
+    Required when type=image.
+
+    A media object of type image.
+
+    Captions not supported when used in a media template.
+    """
+
+    document: Optional[Media] = field(default=None, metadata=config(exclude=lambda f: f is None))
+    """
+    Required when type=document.
+
+    A media object of type document.
+    Only PDF documents are supported for media-based message templates.
+    
+    Captions not supported when used in a media template.
+    """
+
+    video: Optional[Media] = field(default=None, metadata=config(exclude=lambda f: f is None))
+    """
+    Required when type=video.
+
+    A media object of type video.
+    
+    Captions not supported when used in a media template.
     """
