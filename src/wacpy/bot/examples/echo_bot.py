1a81b0c3028bfb32449545973d62e49ba666f107
Natanel Shitrit <natanelsht@gmail.com>
2022-10-17 00:55:42 +0300

    define `WhatsAppEchoBot` as class, implement main for module usage

--- src/wacpy/bot/examples/echo_bot.py
+++ src/wacpy/bot/examples/echo_bot.py
@@ -2,7 +2,7 @@ from wacpy.bot.base_bot import BaseWhatsAppBot
 from wacpy.types import Message, Notification, message, notification
 
 
-def WhatsAppEchoBot(BaseWhatsAppBot):
+class WhatsAppEchoBot(BaseWhatsAppBot):
     """
     The WhatsApp Echo Bot is a sample webhook server application that echoes back to you whatever you send it.
     It can serve as a basic reference for how to set up webhooks and reply to incoming messages.
@@ -35,3 +35,31 @@ def WhatsAppEchoBot(BaseWhatsAppBot):
                 text=message.text
             )
         )
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('phone_id')
+    parser.add_argument('access_token')
+    parser.add_argument('verify_token')
+    parser.add_argument('webhook_path')
+    parser.add_argument('--bind', '-b', metavar='ADDRESS', default='',
+                        help='specify alternate bind address '
+                             '(default: all interfaces)')
+    parser.add_argument('--port', '-p', action='store', default=8000, type=int,
+                        nargs='?',
+                        help='specify alternate port (default: 8000)')
+    args = parser.parse_args()
+
+    WhatsAppEchoBot(
+        phone_id=args.phone_id,
+        access_token=args.access_token
+    ).activate_webhook(
+        bind_address=args.bind,
+        port=args.port,
+        # request handler parameters: 
+        verify_token=args.verify_token,
+        webhook_path=args.webhook_path
+    )
